5c48ac33fbe04043e51cd14e62359f597be9f32d
Timothy Barnes <barnes7td@gmail.com>
2016-01-24 21:31:26 -0500

    Fixed commented out code.

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,8 +1,6 @@
 (function() {
   function AlbumCtrl(Fixtures) {
-    this.albumData = {}; //Fixtures.getAlbum(3);
-    // this.albumData = Fixtures.getAlbum(3);
-    console.log(this.albumData);
+    this.albumData = Fixtures.getAlbum();
   }
   
   angular
